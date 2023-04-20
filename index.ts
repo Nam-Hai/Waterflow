2a48b3bc9e6289363805bfc711377bdcf5f11da0
Lucas TRAN <lucastran00@gmail.com>
2023-04-20 16:35:53 +0200

    index in root

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { FlowProvider } from "./src/FlowProvider";
+import usePageTransition from "./src/composables/usePageTransition";
+import TheBufferPage from './src/components/TheBufferPage.vue';
+
+export { usePageTransition, FlowProvider, TheBufferPage}
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { FlowProvider } from "./FlowProvider";
-import usePageTransition from "./composables/usePageTransition";
-import TheBufferPage from './components/TheBufferPage.vue';
-
-export { usePageTransition, FlowProvider, TheBufferPage}
