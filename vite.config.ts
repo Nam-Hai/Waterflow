bba38d032000e3c5c22937933efd6683a6814d89
Lucas TRAN <lucastran00@gmail.com>
2023-04-20 16:15:45 +0200

    np publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import dts from "vite-plugin-dts";
 import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue()],
    build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
