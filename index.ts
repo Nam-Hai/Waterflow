41e1b83e517e23a098e541d86669031049c90537
Lucas TRAN <lucastran00@gmail.com>
2023-04-22 22:31:42 +0200

    import

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import { FlowProvider } from "./src/FlowProvider";
 import { usePageFlow } from "./src/composables/usePageFlow";
+import type { FlowFunction } from "./src/composables/usePageFlow";
 import BufferPage from './src/components/BufferPage.vue';
 import { onFlow, onBufferFlow } from "./src/composables/onFlow";
 import { provideFlowProvider, useFlowProvider } from "./src/FlowProvider";
 
 export { 
      usePageFlow,
+     FlowFunction,
      FlowProvider,
      BufferPage, 
      onFlow,
