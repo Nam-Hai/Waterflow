282185e5b98896054ec5897d35511bb820e9f749
Lucas TRAN <lucastran00@gmail.com>
2023-04-24 00:34:02 +0200

    index page animations

--- website/pages/index.vue
+++ website/pages/index.vue
@@ -4,11 +4,17 @@
 
         <div class="grid-container">
             <div class="left">
-                <h2>Do you even <span class="font-museo">flow</span> ?</h2>
-                <p>Give your project by the power of the nature. Go with the flow and create transitions that you always
+                <h2><span class="mask-split-line">
+                    <span ref="titleSpanRef">
+                        <span ref="rotateRef">
+                            Do you even <span class="font-museo">flow</span> ?
+                        </span>
+                    </span>
+                </span></h2>
+                <p v-opacity-flow="500">Give your project by the power of the nature. Go with the flow and create transitions that you always
                     dreamed of. Enable your creativity with Waterflow and make your website stand out. A Vue.js library
                     for page transitions crossfade.</p>
-                <h3>Fast. Light. Modular. For <span class="secondary">Vue.js</span>.</h3>
+                <h3 v-opacity-flow="500">Fast. Light. Modular. For <span class="secondary">Vue.js</span>.</h3>
 
                 <Clipboard />
             </div>
@@ -17,6 +23,36 @@
 </template>
 
 <script lang="ts" setup>
+import { vOpacityFlow } from '@/directives/OpacityFlow'
+import { onFlow } from '~/../src/composables/onFlow';
+
+const { $TL } = useNuxtApp()
+const titleSpanRef = ref()
+const rotateRef = ref()
+
+onFlow(()=>{
+    let tl = new $TL()
+    tl.from({
+        el: titleSpanRef.value,
+        d: 1500,
+        p: {
+            y: [100, 0],
+            x: [-2,0],
+            // rotateX: [70,0]
+        },
+        e: 'o3'
+    }).from({
+        el: rotateRef.value,
+        d: 1000,
+        p: {
+            // y: [100, 0],
+            // x: [-2,0],
+            rotateX: [-90,0]
+        },
+        e: 'o3'
+    }).play()
+})
+
 </script>
 
 <style scoped lang="scss">
@@ -29,6 +65,8 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    perspective: 1000px;
 }
 
 .grid-container {
@@ -44,6 +82,17 @@
     }
 }
 
+span {
+    display: inline-block;
+}
+
+.mask-split-line {
+    display: inline-block;
+    height: 9rem;
+    > span {
+        perspective: 1000px;
+    }
+}
 h2 {
     font-size: 8rem;
     font-weight: 700;
@@ -51,7 +100,7 @@ h2 {
 }
 
 p {
-    font-size: 1.6rem;
+    font-size: 2rem;
     font-weight: 400;
     line-height: 100%;
 }
