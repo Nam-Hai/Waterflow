0f9fc33ceafe939b9adb694a8f53fb54af6be0ab
Lucas TRAN <lucastran00@gmail.com>
2023-04-23 21:12:52 +0200

    Directive, animation onMount

---
+++ website/directives/OpacityFlow.js
@@ -0,0 +1,23 @@
+
+// enables v-focus in templates
+
+import { O } from "~/helpers/core/utils"
+
+export const vOpacityFlow = {
+  mounted: (el, binding) => {
+    const {$TL} = useNuxtApp()
+
+    const delay = binding.value
+    O(el, 0)
+
+    let tl = new $TL()
+    tl.from({
+      el,
+      p: {
+        o: [0, 1]
+      },
+      d: 1000,
+      delay
+    }).play()
+  }
+}
