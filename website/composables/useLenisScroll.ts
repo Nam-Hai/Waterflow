61c7958911d770142a729bc9103e364641327060
Lucas TRAN <lucastran00@gmail.com>
2023-04-27 01:49:16 +0200

    useLenisScroll also working out of a component

--- website/composables/useLenisScroll.ts
+++ website/composables/useLenisScroll.ts
@@ -1,31 +1,37 @@
-export const useLenisScroll = (callback: (e:{current: number, target: number, velocity: number}) => void) => {
+export const useLenisScroll = (callback: (e: { current: number, target: number, velocity: number }) => void, component: boolean = true) => {
   const { $lenis } = useNuxtApp()
 
   const onScrollSubscription = ref()
-  onMounted(() => {
-    onScrollSubscription.value = $lenis.on('scroll', callbackProxy)
-    $lenis.emit()
-  })
-  onBeforeUnmount(() => {
-    onScrollSubscription.value()
-    // $lenis.off('scroll', callback)
-  })
 
-  const callbackProxy = (e:any)=>{
+  if (component) {
+    onMounted(() => {
+      onScrollSubscription.value = $lenis.on('scroll', callbackProxy)
+      $lenis.emit()
+    })
+    onBeforeUnmount(() => {
+      console.log(onScrollSubscription.value)
+      onScrollSubscription.value()
+    })
+  }
+
+  const callbackProxy = (e: any) => {
     const arg = {
       current: e.animatedScroll,
       target: e.targetScroll,
       velocity: e.velocity
     }
-    
+
     callback(arg)
   }
 
   const on = () => {
-    onScrollSubscription.value()
-    onScrollSubscription.value = $lenis.on('scroll', callback)
+    onScrollSubscription.value && onScrollSubscription.value()
+    onScrollSubscription.value = $lenis.on('scroll', callbackProxy)
   }
-  const off = () => onScrollSubscription.value()
+  const off = () => {
+    if(!onScrollSubscription.value) return
+    onScrollSubscription.value()
+  }
 
   return {
     lenis: {
