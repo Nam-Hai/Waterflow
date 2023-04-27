341e3af3b2d47e9ac91a82d8fc1c4cc052ac716a
Lucas TRAN <lucastran00@gmail.com>
2023-04-28 00:05:49 +0200

    useraf revert

--- website/composables/useRaf.ts
+++ website/composables/useRaf.ts
@@ -1,19 +1,19 @@
 import { RafR, rafCbType } from "../plugins/core/raf";
 
-export const useRaf = (cb: (e: rafCbType) => void, component: boolean = true) => {
+export const useRaf = (cb: (e: rafCbType) => void) => {
   const { $RafR } = useNuxtApp()
 
-  const raf = new $RafR(cb)
+  const raf = ref() as Ref<RafR>
 
-  if (component) {
-    onMounted(() => {
-      raf.run()
-    })
+  onMounted(() => {
+    raf.value = new $RafR(cb)
+    raf.value.run()
+  })
 
-    onBeforeUnmount(() => {
-      raf.stop()
-    })
-  } 
+  onBeforeUnmount(() => {
+    raf.value.stop()
+  })
 
   return raf
+
 }
