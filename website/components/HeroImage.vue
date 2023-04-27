34c6c3c18dac3417ef51584f957be9357a367830
Lucas TRAN <lucastran00@gmail.com>
2023-04-27 15:25:00 +0200

    heroImage

---
+++ website/components/HeroImage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hero-wrapper">
+    <img class="hero" src="images/dummy.jpg">
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onFlow } from "../../index"
+
+onFlow(()=>{
+})
+
+</script>
+
+<style lang="scss">
+
+.hero-wrapper {
+    flex: 1;
+    position: relative;
+
+    img {
+        border-radius: 4rem;
+        object-fit: cover;
+        position: absolute;
+        top: 0;
+        width: 100%;
+        height: 100%;
+    }
+}
+
+</style>
