3edda702c49b232578c2c3efe9ceaeb99b19e77a
Lucas TRAN <lucastran00@gmail.com>
2023-04-27 02:05:53 +0200

    offset the pass and not the text to have better fluid sim

--- website/scene/Components/TitleMSDF.js
+++ website/scene/Components/TitleMSDF.js
@@ -1,4 +1,4 @@
-import { Geometry, Mesh, Text, Texture, Transform, Program } from 'ogl';
+import { Geometry, Mesh, Text, Texture, Transform, Program, Plane, Camera} from 'ogl';
 import { basicVer } from '../shaders/BasicVer';
 import PostProcessor from '../PostProcessor';
 import FluidPass from '../FluidPass';
@@ -9,6 +9,9 @@ export default class TitleMSDF {
     this.canvasSize = useCanvasSize()
 
 
+    this.scene = new Transform()
+    const { $ROR } = useNuxtApp()
+    this.ro = new $ROR(this.resize.bind(this))
 
     const fluidPass = new FluidPass(this.gl, {
       densityDissipation: 0.98,
@@ -17,33 +20,28 @@ export default class TitleMSDF {
       radius: 0.4,
       // enabled: false
     })
-    this.post = new PostProcessor(this.gl, { targetOnly: true })
+    this.fluidPass = fluidPass
+    this.post = new PostProcessor(this.gl, { 
+      targetOnly: true, 
+      camera: new Camera(this.gl), 
+      geometry: new Plane(this.gl),
+    })
     this.post
       .addPassEffect(fluidPass)
 
-    this.scene = new Transform()
-    const { $RafR, $ROR } = useNuxtApp()
-    this.ro = new $ROR(this.resize.bind(this))
-    this.raf = new $RafR(this.update.bind(this))
 
     this.init()
   }
   async init() {
     await this.loadText()
 
-    this.raf.run()
     this.ro.on()
     this.ro.trigger()
   }
 
-
-  update({ elapsed, delta }) {
-    if (!this.mesh) return
-    // this.mesh.scale.y = Math.sin(elapsed / 100)
-    // this.mesh.scale.x = Math.sin(elapsed / 1000)
-  }
-
   resize({ vh, vw, scale }) {
+    // this.post?.resize({vh,vw})
+
     const w = scale
     const h = scale
     if (!this.mesh) return
@@ -54,7 +52,6 @@ export default class TitleMSDF {
   }
 
   async loadText() {
-    console.log('loadlText')
     const gl = this.gl
     const texture = new Texture(gl, {
       generateMipmaps: false,
@@ -85,7 +82,7 @@ export default class TitleMSDF {
       // width: 4,
       // width: 1,
       align: 'center',
-      // lineHeight: 1,
+      lineHeight: 1,
       // letterSpacing: -0.02,
       size: 300 * this.canvasSize.value.height / innerHeight,
       lineHeight: 1,
@@ -103,22 +100,23 @@ export default class TitleMSDF {
 
     const mesh = new Mesh(gl, { geometry, program });
 
+    this.fluidPass.pass.mesh.position.y = this.canvasSize.value.height / 2 + 20 * this.canvasSize.value.height / innerHeight
+    // mesh.position.y = this.canvasSize.value.height / 2 + 20 * this.canvasSize.value.height / innerHeight
     // Use the height value to position text vertically. Here it is centered.
-    mesh.position.y = this.canvasSize.value.height / 2 + 20 * this.canvasSize.value.height / innerHeight
-    // mesh.position.y += text.height * 0.5;
+      // mesh.position.y += text.height * 0.25;
 
 
 
     this.mesh = mesh
 
     this.mesh.setParent(this.scene)
-    console.log('this.mesh', this.mesh)
 
   }
 
   destroy() {
-    this.raf.stop()
-
+    this.ro.off()
+    this.fluidPass.ro.off()
+    this.post.destroy()
   }
 }
 const fragment = /* glsl */ `#version 300 es
