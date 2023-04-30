23228623624f5115958936e59fb31729a0ce5fba
Lucas TRAN <lucastran00@gmail.com>
2023-04-30 15:26:45 +0200

    slice

--- website/components/Slice/slice1.vue
+++ website/components/Slice/slice1.vue
@@ -1,20 +1,57 @@
 <template>
     <div class="slice wrapper">
 
+        <div class="container">
+            <h2>Create smooth page transition</h2>
+            <p>Sometimes, Vue.js API limits your creativity and imagination to ship outstanding page transition. Enable user
+                to
+                flow between pages to create the most smooth experience possible.</p>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-useLenisScroll(()=>{
+useLenisScroll(() => {
 })
 
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
+@use '@/styles/app/colors.scss' as *;
+
+$bg: #E2383B;
+$soft: #C12B2D;
+
 .wrapper.slice {
     position: relative;
-    height: 300vh;
-    width: 100vh;
-    // background-color: red;
+    height: 100vh;
+    width: 100vw;
+
+
+    .container {
+        padding-top: 28rem;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-family: "HelveticaNeue";
+        text-transform: uppercase;
+        flex-direction: column;
+        text-align: center;
+        font-weight: 700;
+
+        h2 {
+            width: 121.9rem;
+            font-size: 12.8rem;
+            color: $secondary;
+        }
+
+        p {
+            font-size: 3.2rem;
+            width: 82.3rem;
+            color: $soft;
+        }
+    }
+
 }
 </style>
