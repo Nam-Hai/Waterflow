85d0ee96d6ffb859cbae1009ef6208e2aa577815
Lucas TRAN <lucastran00@gmail.com>
2023-05-10 16:25:28 +0200

    website npm version

--- website/app.vue
+++ website/app.vue
@@ -13,19 +13,16 @@ import index from '@/pages/index.vue';
 import Example2 from './pages/example2.vue';
 import Example3 from './pages/example3.vue';
 import Example1 from './pages/example1.vue';
-// import home from '@/pages/home.vue';
 
 const flowProvider = new FlowProvider()
 
 provideFlowProvider(flowProvider)
-
 provide('from-preloader', {value: true})
 
 flowProvider.registerPage('index', index)
 flowProvider.registerPage('example2', Example2)
 flowProvider.registerPage('example3', Example3)
 flowProvider.registerPage('example1', Example1)
-// flowProvider.registerPage('home', home)
 
 onMounted(() => {
 })
